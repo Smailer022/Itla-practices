398a60e68fdca1d31ca1e0a934e7bd932c3e2e87
Welyn Cespedes <20240223@itla.edu.do>
2025-02-08 15:16:24 -0400

    Tarea 2 Programa Sencillo

---
+++ Tarea-2 Cree un programa sencillo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class tarea2
+{
+    public static void Main()
+    {
+        Console.WriteLine("Escribe un numero: ");
+       
+        int Numero = Convert.ToInt32 (Console.ReadLine());
+
+        Console.WriteLine("Resultado: " + Numero);
+
+        if ((Numero % 2) == 0)
+        {
+            Console.WriteLine("Es par");
+        }
+        else
+        {
+            Console.WriteLine("Es impar");
+        }
+    }
+}
+
